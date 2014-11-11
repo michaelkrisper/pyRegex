c8ba3d1e012b67209b8cd5bbe3284fcdf042e1c0
Michael Krisper <michael.krisper@gmail.com>
2014-11-11 17:29:43 +0100

    Initial commit

---
+++ regex.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# coding=utf-8
+"""
+Regex Tutorial
+https://www.youtube.com/watch?v=hiDg2TFYL9E&index=1&list=WL
+"""
+
+__author__ = "Michael Krisper"
+__email__ = "michael.krisper@gmail.com"
+__date__ = "2014-11-11"
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
